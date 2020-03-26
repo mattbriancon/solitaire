ffade88a43e78a5e4c9edf3470dcb9ea476d118c
Matt Briancon <mattbriancon@users.noreply.github.com>
2020-03-25 20:27:26 -0400

    Clearing and re-dealing

--- game.jsx
+++ game.jsx
@@ -11,6 +11,21 @@ function shuffled(array) {
   return shuffledArray;
 }
 
+function* enumerate(iterable, start = 0, step = 1) {
+  let index = start;
+
+  for (const a of iterable) {
+    yield [index, a];
+    index += step;
+  }
+}
+
+function extend(arr1, arr2) {
+  for (const a of arr2) {
+    arr1.push(a);
+  }
+}
+
 class Card {
   static RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
   static SUITS = new Map([
@@ -79,8 +94,14 @@ class Deck {
 
   static nextLowestSuitedCard(card) {
     const rankIndex = Card.RANKS.indexOf(card.rank);
-    const nextLowestRank = Card.RANKS[rankIndex - 1];
-    return Deck.CARDS_BY_KEY.get(Card.makeKey(nextLowestRank, card.suit));
+    const nextRank = Card.RANKS[rankIndex - 1];
+    return Deck.CARDS_BY_KEY.get(Card.makeKey(nextRank, card.suit));
+  }
+
+  static nextHighestSuitedCard(card) {
+    const rankIndex = Card.RANKS.indexOf(card.rank);
+    const nextRank = Card.RANKS[rankIndex + 1];
+    return Deck.CARDS_BY_KEY.get(Card.makeKey(nextRank, card.suit));
   }
 
   static getCardsByRank(rank) {
@@ -101,14 +122,15 @@ class Deck {
 }
 
 class Game {
-  constructor(stock, rows) {
+  constructor(stock, rows, meta) {
     this.stock = stock || Deck.all();
     this.rows = rows || [[], [], [], []];
+    this.meta = meta || { deals: 0 };
   }
 
   _clone() {
     // "clone" is enough to get react to re-render state on change and that's all we're after
-    return new Game(this.stock, this.rows);
+    return new Game(this.stock, this.rows, this.meta);
   }
 
   getRows() {
@@ -119,27 +141,84 @@ class Game {
     return this.stock.length !== 0;
   }
 
-  deal() {
+  cardToLeft(card) {
+    return this.rows[card.row][card.column - 1];
+  }
+
+  cardToRight(card) {
+    return this.rows[card.row][card.column + 1];
+  }
+
+  clearUnfinished() {
     const self = this._clone();
 
+    for (const [rowIndex, row] of enumerate(self.rows)) {
+      if (row.length === 0) continue;
+      let lastGoodCard = row[0];
+
+      // if the first card isn't a 2, add everything to the stock, kill the row, and move on
+      if (lastGoodCard.rank !== "2") {
+        extend(self.stock, row);
+        self.rows[rowIndex] = [];
+        continue;
+      }
+
+      while (self.cardToRight(lastGoodCard) === Deck.nextHighestSuitedCard(lastGoodCard)) {
+        lastGoodCard = Deck.nextHighestSuitedCard(lastGoodCard);
+      }
+
+      extend(self.stock, row.slice(lastGoodCard.column + 1));
+      self.rows[rowIndex] = row.slice(0, lastGoodCard.column + 1);
+    }
+
     Deck.addAces();
+    for (const card of self.stock) {
+      card.row = card.column = null;
+    }
+
+    return self;
+  }
+
+  deal() {
+    if (this.getMovableCards().size > 0) return this;
+
+    const self = this.clearUnfinished();
     self.stock = shuffled(self.stock);
 
-    let rowIndex = 0;
-    for (const row of self.rows) {
+    for (const [rowIndex, row] of enumerate(self.rows)) {
       while (row.length < 13) {
         const card = self.stock.shift();
         card.row = rowIndex;
         card.column = row.length;
         row.push(card);
       }
-
-      rowIndex++;
     }
 
     Deck.removeAces();
+    self.meta.deals++;
 
     return self;
+  }
+
+  getMovableCards() {
+    const blanks = Deck.getBlanks();
+
+    const movable = new Set();
+
+    for (const blank of blanks) {
+      if ([blank.row, blank.column].includes(null)) continue;
+
+      if (blank.column === 0) {
+        for (const two of Deck.getCardsByRank("2")) movable.add(two);
+      } else {
+        const cardToLeft = this.rows[blank.row][blank.column - 1];
+        if (!cardToLeft.isBlank() && cardToLeft.rank !== "K") {
+          movable.add(Deck.nextHighestSuitedCard(cardToLeft));
+        }
+      }
+    }
+
+    return movable;
   }
 
   _swap(card, dest) {
@@ -163,16 +242,13 @@ class Game {
 
     if (card.rank === "2") {
       // find all the blanks in the first column and sort by row
-      const blanksFirstColumn = blanks
-        .filter((c) => c.column === 0)
-        .sort((a, b) => a.row - b.row);
+      const blanksFirstColumn = blanks.filter((c) => c.column === 0).sort((a, b) => a.row - b.row);
 
       if (blanksFirstColumn.length > 0) {
         if (card.column !== 0) {
           // the 2 isn't already in the first column so just put it
           // in the highest available row (lowest row number) in the 0th column
           self._swap(card, blanksFirstColumn[0]);
-
         } else {
           // in this case we want to move the 2 down to the next blank
           // in the first column (wrapping back to the first row if necessary)
@@ -210,13 +286,17 @@ class Game {
 
 function CardComponent({ card, moveCard }) {
   const classes = ["card"];
-  let onClick = null;
 
   if (!card.isBlank()) {
     classes.push(card.color);
-    onClick = () => moveCard(card);
   } else {
     classes.push("blank");
+  }
+
+  let onClick = null;
+  if (moveCard) {
+    onClick = () => moveCard(card);
+    classes.push("movable");
   }
 
   return (
@@ -235,6 +315,7 @@ function CardComponent({ card, moveCard }) {
 
 function Board() {
   const [game, setGame] = React.useState(new Game());
+  const movableCards = game.getMovableCards();
 
   const moveCard = (card) => {
     console.log(`Try to move ${card.key}`);
@@ -244,15 +325,25 @@ function Board() {
   return (
     <div className="board">
       <div className="controls">
-        <button onClick={() => setGame(game.deal())}>Deal</button>
-        <button onClick={() => setGame(game.clearUnset())}>Clear unset</button>
+        <button onClick={() => setGame(new Game().deal())}>New Game</button>|||||||||
+        <button onClick={() => setGame(game.deal())} disabled={movableCards.size > 0}>
+          Deal
+        </button>
+        <button onClick={() => setGame(game.clearUnfinished())}>Clear unset</button>
+        <span>Deals: {game.meta.deals}</span> | <span>Possible moves: {movableCards.size}</span>
       </div>
 
       <div className="rows">
         {game
           .getRows()
           .map((row) =>
-            row.map((card) => <CardComponent card={card} key={card.key} moveCard={moveCard} />),
+            row.map((card) => (
+              <CardComponent
+                card={card}
+                key={card.key}
+                moveCard={movableCards.has(card) ? moveCard : null}
+              />
+            )),
           )
           .flat()}
       </div>
